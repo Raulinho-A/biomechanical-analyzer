aea3a37e6ccddc6c9687c50cde0ce58c90abcb5c
Raulinho-A <raul0207abanto@gmail.com>
2025-04-19 20:50:01 -0500

    extract keypoints

---
+++ analyzer/vision/preprocess_video.py
@@ -0,0 +1,33 @@
+import cv2
+
+OUTPUT_PATH = 'data/raw/record_01.mp4'
+
+cap = cv2.VideoCapture(0)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+if not cap.isOpened():
+    raise IOError(f"Cannnot open camera")
+
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(OUTPUT_PATH, fourcc, fps, (frame_width, frame_height))
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    out.write(frame)
+    cv2.imshow('Record:', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+    
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+print(f"Video guardado en: {OUTPUT_PATH}")
