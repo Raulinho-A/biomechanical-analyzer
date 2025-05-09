6cb982c8b5db14f2b831db58c2141138ea4ee001
Raulinho-A <raul0207abanto@gmail.com>
2025-05-09 18:00:03 -0500

    feature: Pose Drawer

--- analyzer/core/main.py
+++ analyzer/core/main.py
@@ -1,6 +1,7 @@
 from analyzer.vision.capture import VideoRecorder
 from analyzer.vision.keypoints.pose_extractor import PoseExtractor
-from analyzer.config import DEMOS
+from analyzer.vision.keypoints.pose_drawer import PoseDrawer
+from analyzer.config import DEMOS, KEYPOINTS
 
 def main():
     ## Recording
@@ -8,9 +9,16 @@ def main():
     # recorder.record()
 
     ## Extracting
+    # video_path = DEMOS / 'demo_01.mp4'
+    # extractor = PoseExtractor(video_path=video_path)
+    # extractor.extract(show=True)
+
+    ## Drawing
     video_path = DEMOS / 'demo_01.mp4'
-    extractor = PoseExtractor(video_path=video_path)
-    extractor.extract(show=True)
-    
+    keypoints_json = KEYPOINTS / 'demo_01_keypoints.json'
+
+    drawer = PoseDrawer(video_path=video_path, keypoints_json=keypoints_json)
+    drawer.draw_and_save(show=True)
+
 if __name__ == "__main__":
     main()
