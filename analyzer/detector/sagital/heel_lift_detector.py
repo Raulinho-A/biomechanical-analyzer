0cd0eb82b452fc2e418ded08b8f78deec6f6bff2
Raulinho-A <raul0207abanto@gmail.com>
2025-05-11 18:06:37 -0500

    feat: heel lift detector

--- analyzer/detector/sagital/heel_lift_detector.py
+++ analyzer/detector/sagital/heel_lift_detector.py
@@ -1,6 +1,5 @@
 from analyzer.detector.error_detector import PoseLandmark, PostureErrorDetector
 from analyzer.config.thresholds import HEEL_LIFT_Y_THRESHOLD
-import numpy as np
 
 class HeelLiftDetector(PostureErrorDetector):
     def evaluate(self):
@@ -10,6 +9,9 @@ class HeelLiftDetector(PostureErrorDetector):
         right_heel_y = self.get_point(PoseLandmark.RIGHT_HEEL)[1]
         right_toe_y = self.get_point(PoseLandmark.RIGHT_FOOT_INDEX)[1]
 
+        left_score = left_toe_y - left_heel_y
+        right_score = right_toe_y - right_heel_y
+
         left_elevated = (left_heel_y < left_toe_y - HEEL_LIFT_Y_THRESHOLD)
         right_elevated = (right_heel_y < right_toe_y - HEEL_LIFT_Y_THRESHOLD)
 
@@ -18,6 +20,8 @@ class HeelLiftDetector(PostureErrorDetector):
             "left_toe_y": left_toe_y,
             "right_heel_y": right_heel_y,
             "right_toe_y": right_toe_y,
+            "heel_lift_score_left": left_score,
+            "heel_lift_score_right": right_score,
             "left_heel_elevated": left_elevated,
             "right_heel_elevated": right_elevated,
             "heel_lift_detected_in": (
